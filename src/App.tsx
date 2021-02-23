2b919d20afb35d4a0955b7616bd2d4ef13ea961a
Rod <rod.bromberg@yahoo.com>
2021-02-23 10:47:04 -0500

    base-files

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Practice from './Practice';
+
+function App() {
+  return (
+    <div className="App">
+   
+      <Practice />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Practice.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const Practice = () => {
+
+const jim = 'hi'
+
+return (
+    <div>
+        {typeof jim}
+    </div>
+)
+
+}
+
+export default Practice
